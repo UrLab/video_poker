1cf1482ed5fb8211af5156f23600ff79df16f02f
loispostula <lois.postula@railnova.eu>
2016-06-16 23:07:45 +0200

    add basic card function

--- deck/card.py
+++ deck/card.py
@@ -1,4 +1,18 @@
 
 
 class Card(object):
-    pass
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+    def get_rank(self):
+        return self.rank
+
+    def get_suit(self):
+        return self.suit
+
+    def set_rank(self, rank):
+        self.rank = rank
+
+    def set_suit(self, suit):
+        self.suit = suit
--- tests/deck/test_card.py
+++ tests/deck/test_card.py
@@ -1,12 +1,22 @@
+import pytest
 from deck.card import Card
 
 
-def assert_card(card):
-    # prove the card is valid
-    pass
+def test_cards_creation():
+    for suit in ['heart', 'diamond', 'spade', 'club']:
+        for rank in range(1, 14):
+            card = Card(rank, suit)
+            assert rank == card.get_rank()
+            assert suit == card.get_suit()
 
-def test_basic_cards():
-    for suit in ['hearts', 'diamonds', 'spades', 'clubs']:
-        for i in range(1, 14):
-            card = Card(i, suit)
-            assert_card(card)
+
+def test_card_rank_setting():
+    card = Card(1, 'heart')
+    card.set_rank(2)
+    assert 2 == card.get_rank()
+
+
+def test_card_suit_setting():
+    card = Card(1, 'heart')
+    card.set_suit('club')
+    assert 'club' == card.get_suit()
