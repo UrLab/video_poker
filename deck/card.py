a048ab9adc89e2348628e304069e8ba71b389e74
loispostula <lois.postula@railnova.eu>
2016-06-16 23:16:46 +0200

    basic card creation

--- deck/card.py
+++ deck/card.py
@@ -2,8 +2,16 @@
 
 class Card(object):
     def __init__(self, rank, suit):
+        self._test_card_validity(rank, suit)
         self.rank = rank
         self.suit = suit
+
+    @staticmethod
+    def _test_card_validity(rank, suit):
+        if rank < 1 or rank > 13:
+            raise ValueError
+        if suit not in ['heart', 'diamond', 'spade', 'club']:
+            raise ValueError
 
     def get_rank(self):
         return self.rank
@@ -12,7 +20,9 @@ class Card(object):
         return self.suit
 
     def set_rank(self, rank):
+        self._test_card_validity(rank, self.suit)
         self.rank = rank
 
     def set_suit(self, suit):
+        self._test_card_validity(self.rank, suit)
         self.suit = suit
--- tests/deck/test_card.py
+++ tests/deck/test_card.py
@@ -20,3 +20,28 @@ def test_card_suit_setting():
     card = Card(1, 'heart')
     card.set_suit('club')
     assert 'club' == card.get_suit()
+
+
+def test_invalid_rank_rejected():
+    with pytest.raises(ValueError):
+        Card(0, 'club')
+        Card(14, 'club')
+
+
+def test_invalid_suit_rejected():
+    with pytest.raises(ValueError):
+        Card(1, 'nonheart')
+        Card(1, 'hearts')
+        Card(1, 'hear')
+
+
+def test_invalid_rank_set_rejected():
+    card = Card(1, 'heart')
+    with pytest.raises(ValueError):
+        card.set_rank(14)
+
+
+def test_invalid_suit_set_rejected():
+    card = Card(1, 'heart')
+    with pytest.raises(ValueError):
+        card.set_suit('he')
